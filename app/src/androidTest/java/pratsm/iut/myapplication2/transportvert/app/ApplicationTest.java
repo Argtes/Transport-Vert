fa9e55e5cdcb0cfd5365c6b1f63b2902a6962f44
Argtes <stfu.gtfo@laposte.net>
2015-10-17 14:04:29 +0200

    initial commit

---
+++ app/src/androidTest/java/pratsm/iut/myapplication2/transportvert/app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pratsm.iut.myapplication2.transportvert.app;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
